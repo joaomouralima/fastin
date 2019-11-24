861cd8ae219bd52750365ca2014da39b6f78b229
João Moura Lima <jnmlima92@gmail.com>
2019-11-24 08:23:23 -0300

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'PaymentX';
+  title = 'FastIn';
+  url_avatar = '../assets/avatar1.png'
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FontAwesomeModule, FaIconLibrary, } from '@fortawesome/angular-fontawesome';
+
+import { faSquare, faCheckSquare, faCocktail, faGuitar, faFilm, faFemale } from '@fortawesome/free-solid-svg-icons';
 
 @NgModule({
   declarations: [
@@ -10,9 +14,15 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(private library: FaIconLibrary) {
+    library.addIcons(faSquare, faCheckSquare, faCocktail, faGuitar, faFilm, faFemale);
+  }
+}
